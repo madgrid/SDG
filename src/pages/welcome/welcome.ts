917141c4fa8adc19259ea263128c3639867e3211
madgrid <aaronmadved@gmail.com>
2018-02-06 15:04:12 -0500

    Lazy loading for WelcomePage

--- src/pages/welcome/welcome.ts
+++ src/pages/welcome/welcome.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { IonicPage, NavController } from 'ionic-angular';
 
-import {SignupSelectPage} from '../signup-select/signup-select';
+// import {SignupSelectPage} from '../signup-select/signup-select';
 
 /**
  * The Welcome Page is a splash page that quickly describes the app,
  * and then directs the user to create an account or log in.
  * If you'd like to immediately put the user onto a login/signup page,
  * we recommend not using the Welcome page.
-*/
+ */
+
+@IonicPage()
 @Component({
   selector: 'page-welcome',
   templateUrl: 'welcome.html'
@@ -22,6 +24,6 @@ export class WelcomePage {
   }
 
   signup() {
-    this.navCtrl.push(SignupSelectPage);
+    this.navCtrl.push('SignupSelectPage');
   }
 }
